92b9aad6a98db180ee5b7dc6eddcbe401a3a87a0
Francis <francis.beeson@sharktower.com>
2022-05-17 13:08:41 +0100

    Refactor solution

--- main.py
+++ main.py
@@ -1,50 +1,64 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
 import time
+
 
 chrome_driver_path = "C:\Drivers\chromedriver.exe"
 driver = webdriver.Chrome(service=Service(chrome_driver_path))
 
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
-cookie = driver.find_element(By.ID, "cookie")
-money_element = driver.find_element(By.ID,"money")
 
-buy_cursor = driver.find_element(By.ID, "buyCursor")
-buy_grandma = driver.find_element(By.ID, "buyGrandma")
-buy_factory = driver.find_element(By.ID, "buyFactory")
-buy_mine = driver.find_element(By.ID, "buyMine")
-buy_shipment = driver.find_element(By.ID, "buyShipment")
-buy_alchemy = driver.find_element(By.ID, "buyAlchemy lab")
-buy_portal = driver.find_element(By.ID, "buyPortal")
-buy_time_machine = driver.find_element(By.ID, "buyTime machine")
-buy_elder_pledge = driver.find_element(By.ID, "buyElder Pledge")
+def get_available_producers(driver: WebDriver):
+    producers = driver.find_elements(By.CSS_SELECTOR, "#store div")
+    available_producers = [producer for producer in producers if producer.get_attribute("class") == ""]
+    return available_producers
 
-producers = [buy_cursor, buy_grandma, buy_factory, buy_mine, buy_shipment, buy_alchemy, buy_portal, buy_time_machine, buy_elder_pledge]
 
 def get_producer_price(producer):
     price = int(producer.find_element(By.TAG_NAME, "b").text.split("-")[-1].strip())
     return price
 
+
+def get_available_prices(driver: WebDriver):
+    producers = driver.find_elements(By.CSS_SELECTOR, "#store div")
+    available_prices = [get_producer_price(producer) for producer in producers if producer.get_attribute("class") == ""]
+    return available_prices
+
+
+def get_element_by_id(driver: WebDriver, id: str):
+    element = driver.find_element(By.ID, id)
+    return element
+
+
+game_duration = 30
 check_interval = 3
 
-# while True:
-time_start = time.time()
-while time.time() < time_start + check_interval:
-    cookie.click()
-money = int(money_element.text)
-print(f"Money: {money}") #TEST
-for producer in reversed(producers):
-    producer_type = producer.get_attribute("id")
-    print(f"Producer: {producer_type}") #TEST
-    if producer.get_attribute("class") == "grayed":
-        continue
-    price = get_producer_price(producer)
-    print(f"Price: {price}") #TEST
-    if money > price:
-        producer.click()
+game_time_start = time.time()
+while time.time() < game_time_start + game_duration:
+    
+    cookie = get_element_by_id(driver, "cookie")
+    interval_time_start = time.time()
+    while time.time() < interval_time_start + check_interval:
+        cookie.click()
+
+    money = get_element_by_id(driver, "money")
+    money_amount = int(money.text)
+    
+    prices = get_available_prices(driver)
+
+    while money_amount > prices[-1]:
+        producers = get_available_producers(driver)
+        most_expensive_producer = producers.pop()
+        most_expensive_producer.click()
+
+        
+        money = get_element_by_id(driver, "money")
+        money_amount = int(money.text)
+        prices = get_available_prices(driver)
 
 driver.quit()
 
+#TODO: Handle `StaleElementReferenceException`

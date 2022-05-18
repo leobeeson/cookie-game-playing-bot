98d9022e58f1ae2ab3a9d62e937df8e640b5aabe
Francis Beeson <leobeeson@gmail.com>
2022-05-18 12:38:43 +0100

    Refactor getting producers and producers' prices

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from typing import List
+from typing import Tuple
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 import time
 
@@ -11,21 +15,10 @@ driver = webdriver.Chrome(service=Service(chrome_driver_path))
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 
-def get_available_producers(driver: WebDriver):
+def get_available_producers(driver: WebDriver) -> List[tuple]:
     producers = driver.find_elements(By.CSS_SELECTOR, "#store div")
-    available_producers = [producer for producer in producers if producer.get_attribute("class") == ""]
+    available_producers = [(producer, int(producer.find_element(By.TAG_NAME, "b").text.split("-")[-1].strip())) for producer in producers if producer.get_attribute("class") == ""]
     return available_producers
-
-
-def get_producer_price(producer):
-    price = int(producer.find_element(By.TAG_NAME, "b").text.split("-")[-1].strip())
-    return price
-
-
-def get_available_prices(driver: WebDriver):
-    producers = driver.find_elements(By.CSS_SELECTOR, "#store div")
-    available_prices = [get_producer_price(producer) for producer in producers if producer.get_attribute("class") == ""]
-    return available_prices
 
 
 def get_element_by_id(driver: WebDriver, id: str):
@@ -47,17 +40,14 @@ while time.time() < game_time_start + game_duration:
     money = get_element_by_id(driver, "money")
     money_amount = int(money.text)
     
-    prices = get_available_prices(driver)
+    available_producers = get_available_producers(driver)
 
-    while money_amount > prices[-1]:
-        producers = get_available_producers(driver)
-        most_expensive_producer = producers.pop()
-        most_expensive_producer.click()
-
+    while money_amount > available_producers[-1][1]:        
+        available_producers[-1][0].click()
         
         money = get_element_by_id(driver, "money")
         money_amount = int(money.text)
-        prices = get_available_prices(driver)
+        available_producers = get_available_producers(driver)
 
 driver.quit()
 
